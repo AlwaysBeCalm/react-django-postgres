27e6f436878f29ce8cbdcbf9ca4b2a1af68f46c6
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 09:05:17 +0300

    added the employees endpoints/urls

--- backend/EmployeeApp/urls.py
+++ backend/EmployeeApp/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('departments', department_api),
     path('departments/<_id>', department_api),
+
+    path('employees', employee_api),
+    path('employees/<_id>', employee_api),
 ]
