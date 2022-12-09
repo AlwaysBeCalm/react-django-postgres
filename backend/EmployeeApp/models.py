b6919629bd7cdeefaa4e50c93afa819185a2ff4f
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 07:44:42 +0300

    added departments and employees Models/Tables.

--- backend/EmployeeApp/models.py
+++ backend/EmployeeApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Departments(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=50)
+
+
+class Employees(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    Department = models.CharField(max_length=50)
+    JoiningDate = models.DateField()
+    Photo = models.CharField(max_length=500)
