696414855d95763f0039091e3780d7b6d34ab97f
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 09:06:05 +0300

    added a method/view to handle the employees operations

--- backend/EmployeeApp/views.py
+++ backend/EmployeeApp/views.py
@@ -40,3 +40,34 @@ def department_api(request, _id=0):
         department = Departments.objects.get(Id=_id)
         department.delete()
         return JsonResponse("Department Data Deleted Successfully.", safe=False, status=HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def employee_api(request, _id=0):
+    # todo: remaining is getting a single employee.
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_data = EmployeesSerializer(employees, many=True)
+        return JsonResponse(employees_data.data, safe=False, status=HTTP_200_OK)
+
+    elif request.method == 'POST':
+        employee_data = JSONParser().parse(request)
+        employee_data = EmployeesSerializer(data=employee_data)
+        if employee_data.is_valid():
+            employee_data.save()
+            return JsonResponse("New Employee saved successfully", status=HTTP_201_CREATED, safe=False)
+        return JsonResponse("Enter Employee Data correctly", status=HTTP_400_BAD_REQUEST, safe=False)
+
+    elif request.method == 'PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(Id=employee_data["Id"])
+        employee_data = EmployeesSerializer(employee, data=employee_data)
+        if employee_data.is_valid():
+            employee_data.save()
+            return JsonResponse("Updated Successfully", safe=False, status=HTTP_200_OK)
+        return JsonResponse("Enter Employee Data correctly", status=HTTP_400_BAD_REQUEST, safe=False)
+
+    elif request.method == 'DELETE':
+        employee = Employees.objects.get(Id=_id)
+        employee.delete()
+        return JsonResponse("Employee Data Deleted Successfully.", safe=False, status=HTTP_204_NO_CONTENT)
