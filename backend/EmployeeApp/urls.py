2ac3499eb9fb90e2f437a0e5356f2faae39a5737
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 10:55:30 +0300

    added url to save a photo

--- backend/EmployeeApp/urls.py
+++ backend/EmployeeApp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('departments', department_api),
     path('departments/<_id>', department_api),
 
+    path('employees/save_photo', save_photo),
     path('employees', employee_api),
     path('employees/<_id>', employee_api),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
