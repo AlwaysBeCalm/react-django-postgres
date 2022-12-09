ae3659fa48df4d64312b5061393217c4ef35841a
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 15:15:42 +0300

    added functionalities to: create a new department update a department delete a department

--- frontend/src/Components/Department.js
+++ frontend/src/Components/Department.js
@@ -19,19 +19,82 @@ export class Department extends Component {
 
     showModal(obj, title) {
         this.setState({
-            modalTitle: title,
-            Id: obj.Id,
-            Name: obj.Name
-        })
+                modalTitle: title,
+                Id: obj.Id,
+                Name: obj.Name
+            }
+        )
     }
 
     addDepartment() {
-        fetch(api_endpoints.API_URL + "/departments")
+        fetch(api_endpoints.API_URL + "/departments", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                        Name: this.state.Name
+                    }
+                )
+            }
+        )
             .then(response => response.json())
             .then(data => {
-                    this.setState({departments: data})
+                    alert(data)
+                    this.refreshList()
+                    document.getElementsByClassName("btn-close")[0].click()
+                }, (error) => {
+                    alert(error)
                 }
             )
+    }
+
+    updateDepartment() {
+        fetch(api_endpoints.API_URL + "/departments", {
+                method: "PUT",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                        Id: this.state.Id,
+                        Name: this.state.Name
+                    }
+                )
+            }
+        )
+            .then(response => response.json())
+            .then(data => {
+                    alert(data)
+                    this.refreshList()
+                    document.getElementsByClassName("btn-close")[0].click()
+                }, (error) => {
+                    alert(error)
+                }
+            )
+    }
+
+    deleteDepartment(Id) {
+        if (window.confirm("Are you sure?")) {
+            fetch(api_endpoints.API_URL + "/departments/" + Id, {
+                    method: "DELETE",
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json",
+                    },
+                body: JSON.stringify("")
+                }
+            )
+                .then(response => response.json())
+                .then(data => {
+                        alert(data)
+                        this.refreshList()
+                    }, (error) => {
+                        alert(error)
+                    }
+                )
+        }
     }
 
     refreshList() {
@@ -87,7 +150,8 @@ export class Department extends Component {
                                 </button>
                             </td>
                             <td>
-                                <button type="button" className="btn btn-danger mr-1">
+                                <button type="button" className="btn btn-danger mr-1"
+                                        onClick={() => this.deleteDepartment(department.Id)}>
                                     Delete
                                 </button>
                             </td>
@@ -115,14 +179,15 @@ export class Department extends Component {
 
                                 {Id === 0 ?
                                     <button type="button" className="btn btn-primary float-start"
-                                            onClick={this.addDepartment}>
+                                            onClick={() => this.addDepartment()}>
                                         Create
                                     </button>
                                     :
                                     null
                                 }
                                 {Id !== 0 ?
-                                    <button type="button" className="btn btn-primary float-start">
+                                    <button type="button" className="btn btn-primary float-start"
+                                            onClick={() => this.updateDepartment()}>
                                         Update
                                     </button>
                                     :
