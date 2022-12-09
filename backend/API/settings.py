1edfeca225f396327536c986c4463300111f53ee
Abdullah Nasser <abdullah-nasser-@outlook.com>
2022-12-09 07:45:07 +0300

    set the database to postgres instead of sqlite.

--- backend/API/settings.py
+++ backend/API/settings.py
@@ -88,9 +88,17 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HR',
+        'USER': 'HR',
+        'PASSWORD': 'HR',
+        'HOST': 'localhost',
+        'PORT': "5432",
     }
 }
 
