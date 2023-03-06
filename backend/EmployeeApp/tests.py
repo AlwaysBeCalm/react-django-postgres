4c5b2266b51970413eed774ea1d7690ec34ada85
Abdullah Nasser <abdullah-nasser-@outlook.com>
2023-03-06 21:13:51 +0300

    test for department

--- backend/EmployeeApp/tests.py
+++ backend/EmployeeApp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from EmployeeApp.models import Departments
 
 # Create your tests here.
+
+
+class DepartmentTestCase(TestCase):
+
+    def setUp(self) -> None:
+        Departments.objects.create(Name="HR")
+
+    def test_confirm_department_name(self):
+        department = Departments.objects.get(pk=1)
+        self.assertEqual(department.Name, "HR")
